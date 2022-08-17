c5d1465afa51a5a7de8e4a19072718afabedecba
Łukasz Łukaszew <lukasz.lukaszew@gmail.com>
2022-08-17 08:13:06 +0200

    Add ship image to the screen

---
+++ settings.py
@@ -0,0 +1,9 @@
+"""All settings of the game"""
+
+
+class Settings():
+    """Class dedicated to store all game configuration"""
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
---
+++ ship.py
@@ -0,0 +1,18 @@
+"""Module for ship features"""
+
+import pygame
+
+class Ship():
+    """Class representing ship and it's functionalities"""
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """Showing ship in it's current position"""
+        self.screen.blit(self.image, self.rect)
