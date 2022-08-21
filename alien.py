6b46aae56972f7ab78b99f9e10240ac1d991b829
Łukasz Łukaszew <lukasz.lukaszew@gmail.com>
2022-08-21 15:41:41 +0200

    Add fleet movement

--- alien.py
+++ alien.py
@@ -6,6 +6,7 @@ from pygame.sprite import Sprite
 
 class Alien(Sprite):
     """Class representing alien and it's functionalities"""
+
     def __init__(self, ai_settings, screen):
         super().__init__()
         self.screen = screen
@@ -18,6 +19,19 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
 
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move alien to the right"""
+        self.x += self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """Returns true if alien is at hhe edge of the screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        if self.rect.left <= 0:
+            return True
 
     def blitme(self):
         """Show alien in its current position"""
--- game_functions.py
+++ game_functions.py
@@ -99,3 +99,24 @@ def get_number_rows(ai_settings, ship_height, alien_height):
     availables_space_y = ai_settings.screen_height - (3 * alien_height) - ship_height
     number_rows = availables_space_y // (2 * alien_height)
     return number_rows
+
+
+def update_aliens(ai_settings, aliens):
+    """Check screen edges and update position of every alien in the group"""
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+
+def check_fleet_edges(ai_settings, aliens):
+    """Reactiion for alien reaching the edge of the screen"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    """Move down and change direction of horizontal movement"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
